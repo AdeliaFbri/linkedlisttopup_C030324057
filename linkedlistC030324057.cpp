93ad4ec9c5cdc5175eb3ebd9b9bb44dc94912b40
AdeliaFbri <adeliafbria1497@gmail.com>
2025-06-19 19:53:32 +0800

    Struct Data Pelangga

---
+++ linkedlistC030324057.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+using namespace std;
+
+struct Pelanggan {
+    string nama;
+    string namaItem;
+    int hargaItem;
+};
+
+struct Node {
+    Pelanggan data;
+    Node* next;
+};
+
+Node* head = nullptr;
